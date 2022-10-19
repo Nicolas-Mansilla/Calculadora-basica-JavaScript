8a771746978d8ad8f57d4398bbec1d6ed511ff10
Nicolas-Mansilla <nicolasmansilla6284@gmail.com>
2022-10-19 12:35:47 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let num1 = prompt("Ingrese numero 1 para operar")
+let num2 = prompt("Ingrese numero 2 para operar")
+
+
+let suma = parseInt(num1) + parseInt(num2)
+document.write("suma=", suma);
+let resta = parseInt(num1) - parseInt(num2)
+document.write("resta=", resta);
+let div = parseInt(num1) / parseInt(num2)
+document.write("division=", div);
+let multiplicacion = parseInt(num1) * parseInt(num2)
+document.write("multiplicacion=", multiplicacion);
